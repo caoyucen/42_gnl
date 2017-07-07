ecb4e23c10afa3fc584d9f257e0149c357e3d092
ycao <caoyucen@gmail.com>
2017-07-06 20:21:41 -0700

    00

--- get_next_line.c
+++ get_next_line.c
@@ -12,13 +12,38 @@
 
 # include "get_next_line.h"
 
+
+
 int		get_next_line(const int fd, char **line)
 {
+	char	buf[BUFF_SIZE + 1];
+	int		ret;
+	int		a;
+	int		b;
+	int		i;
 
-
-
-
-
-
-	return
+	a = 0;
+	b = 0;
+	if (fd != 1 || !line)
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		i = 0;
+		while (i < ret)
+		{
+			if (buf[i] == '\n')
+			{
+				lina[a][b] = '\0';
+				a++;
+				b = 0;
+				i++;
+			}
+			line[a][b] = buf[i];
+			i++;
+			b++;
+		}
+		line[a][b] = '\0';
+		line[a] = NULL;
+	}
+	return (1);
 }
